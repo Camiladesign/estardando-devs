432bf49f0bdabb044bebf8a6c60d9f10c137d8ab
PabloHonorato <pblhonorato@gmail.com>
2018-09-17 13:54:45 -0300

    corrigindo a merd@ q fiz

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,7 +3,7 @@
         var pacientes = document.querySelectorAll(".paciente");
         
         //Loop que percorre os pacientes da tabela e calcula o IMC
-    for(var i = 0; i < pacientes.length; i = i + 1){
+    for (var i = 0; i < pacientes.length; i = i + 1){
 
         //Variavel referente ao indice do paciente
         var paciente = pacientes [i]; 
@@ -12,7 +12,6 @@
         var tdPeso = paciente.querySelector('.info-peso');
         var tdAltura = paciente.querySelector('.info-altura');
         var tdImc = paciente.querySelector('.info-imc');
-<<<<<<< HEAD
  
         //Variaveis dados da Tabela
         var peso = tdPeso.textContent;
@@ -22,7 +21,6 @@
         var pesoEhValido = validaPeso(peso);
         var alturaEhValido = validaALtura(altura);
 
-=======
 
         //Variaveis dados da Tabela
         var peso = tdPeso.textContent;
@@ -31,7 +29,7 @@
         //Variaveis de validação
         var pesoEhValido = validaPeso(peso);
         var alturaEhValido = validaALtura(altura);
->>>>>>> b72f246fe34a282b91b6f4437838f613f8ef1d30
+
 
 
         //Validação altura e ṕeso
@@ -70,10 +68,7 @@ function validaALtura(altura) {
 }
 
 function calculaImc(peso, altura) {
-<<<<<<< HEAD
-=======
-    
->>>>>>> b72f246fe34a282b91b6f4437838f613f8ef1d30
+
         var imc = 0;
         imc = peso / (altura * altura);
         return imc.toFixed(2);
